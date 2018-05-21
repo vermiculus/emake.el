8c5dbadb90d0b34390b8f83c7d806ff3a735f640
Sean Allred <code@seanallred.com>
2018-05-21 17:55:38 -0500

    Add live testing example

---
+++ sample.el
@@ -0,0 +1,28 @@
+;;; sample.el --- example file for testing with emake  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Sean Allred
+
+;; Author: Sean Allred <code@seanallred.com>
+;; Package-Requires: ((s "0.3"))
+;; Package-Version: 0.1
+;; Keywords: tools, lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defvar sample-foo 'bar)
+
+(provide 'sample)
+;;; sample.el ends here
---
+++ test-sample.el
@@ -0,0 +1,4 @@
+(require 'sample)
+
+(ert-deftest my-test ()
+  (should (eq sample-foo 'bar)))
