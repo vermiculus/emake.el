5be08e9513fb854d2afaabe9dbaf4e8be7944e37
Sean Allred <code@seanallred.com>
2018-05-21 21:15:08 -0500

    Documentation fixes

--- emake.el
+++ emake.el
@@ -1,31 +1,65 @@
-;;; Emacs-Make -- simple, transparent functionality for automated elisp testing
-;;; Based on http://edkolev.github.io/posts/2017-09-10-travis-for-emacs-packages.html
-
-;; PACKAGE_FILE     := the root file of your package
-;; PACKAGE_TESTS    := the root file to load your tests
-;; PACKAGE_LISP     := list of Lisp files in this package separated by spaces
-;; PACKAGE_ARCHIVES := list of named ELPA archives separated by spaces; see also
-;;                     `emake-package-archive-master-alist'
-
-;;; You can debug emacs-make by setting the environment variable EMACS_MAKE_DEBUG_MODE.
-
 (setq debug-on-error (getenv "EMACS_MAKE_DEBUG_MODE"))
+;;; emake.el --- simple, transparent functionality for automated Elisp testing  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Sean Allred
+
+;; Author: Sean Allred <code@seanallred.com>
+;; Keywords: tools, lisp
+;; Homepage: https://github.com/vermiculus/emake.el
+;; Package-Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Based on http://edkolev.github.io/posts/2017-09-10-travis-for-emacs-packages.html
+;;
+;; Emacs-Make is driven by environment variables to know what to do.
+;; Before invoking `emake', ensure the following variables are set
+;; appropriately:
+;;
+;;     PACKAGE_FILE     := the root file of your package
+;;     PACKAGE_TESTS    := the root file to load your tests
+;;     PACKAGE_LISP     := space-delimited list of Lisp files in this package
+;;     PACKAGE_ARCHIVES := space-delimited list of named ELPA archives; see also
+;;                         `emake-package-archive-master-alist'
+;;
+;; To debug Emacs-Make, set the environment variable EMACS_MAKE_DEBUG_MODE.
+;;
+;; See the file README.org in this directory for more information and
+;; minimal examples.
+
+;;; Code:
+
 
 (require 'package)
 (require 'subr-x)
 (require 'cl-lib)
 
 (defun emake-message (format &rest args)
-  "Print a message to standard out"
+  "Print a message to standard out.
+Argument FORMAT is a format string.  Optional argument ARGS is a
+list of arguments for that format string."
   (apply #'message (concat "emake: " format) args))
 
 (defconst emake-package-file
   (getenv "PACKAGE_FILE")
-  "The Elisp file with package headers")
+  "The Elisp file with package headers.")
 
 (defconst emake-package-tests-file
   (getenv "PACKAGE_TESTS")
-  "The Elisp file with test definitions")
+  "The Elisp file with test definitions.")
 
 (defvar emake-package-desc
   (with-temp-buffer
@@ -36,22 +70,24 @@
   (locate-dominating-file (or (file-name-directory emake-package-file)
                               default-directory)
                           emake-package-file)
-  "The folder `emake-package-file' is in")
+  "The folder `emake-package-file' is in.")
 
 (defconst emake-package-archives
   (when-let ((deps (getenv "PACKAGE_ARCHIVES")))
     (thread-last (split-string deps nil 'omit-nulls)
       (mapcar #'downcase)))
-  "List of names of archives in use by the package
+  "A list of names of archives in use by the package.
 Keys in `emake-package-archive-master-alist'.")
 
 (defconst emake-package-archive-master-alist
   '(("melpa" . "http://melpa.org/packages/")
     ("gnu" . "http://elpa.gnu.org/packages/"))
-  "Alist of archive-names to their locations")
+  "Archive definition alist.
+Key is the string name of the archive.
+Value is the URL at which the archive is hosted.")
 
 (defmacro emake-task (description &rest body)
-  "Run BODY wrapped by DESCRIPTION messages"
+  "Run BODY wrapped by DESCRIPTION messages."
   (declare (indent 1))
   (let ((Sdescription (cl-gensym)))
     `(let ((,Sdescription (concat ,description "...")))
@@ -61,7 +97,7 @@ Keys in `emake-package-archive-master-alist'.")
          (emake-message (concat ,Sdescription "done"))))))
 
 (defmacro emake-with-elpa (&rest body)
-  "Run BODY after setting up ELPA context"
+  "Run BODY after setting up ELPA context."
   (declare (debug t))
   `(let ((package-user-dir (expand-file-name (format ".elpa/%s/elpa" emacs-version)
                                              emake-project-root))
@@ -74,7 +110,7 @@ Keys in `emake-package-archive-master-alist'.")
      ,@body))
 
 (defun emake (target)
-  "Run `emake-TARGET' if bound."
+  "Run `emake-my-TARGET' if bound, else `emake-TARGET'."
   (let ((fun (intern (format "emake-my-%s" target))))
     (unless (fboundp fun)
       (setq fun (intern (format "emake-%s" target))))
@@ -115,10 +151,13 @@ dependencies."
            (package-install package)))))))
 
 (defun emake-compile ()
-  "Compile all files in PACKAGE_LISP"
+  "Compile all files in PACKAGE_LISP."
   (require 'bytecomp)
   (emake-with-elpa
    (add-to-list 'load-path emake-project-root)
    (dolist (f (split-string (getenv "PACKAGE_LISP") nil 'omit-nulls))
      (emake-task (format "compiling %s" f)
        (byte-compile-file f)))))
+
+(provide 'emake)
+;;; emake.el ends here
