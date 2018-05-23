vermiculus/emake.el

commit 9a2c7716f41a5bff6723f3920d9e025633cb6796

Add `melpa-stable' and `org' ELPA definitions

emake.el
@@ -87,8 +87,10 @@ list of arguments for that format string."
     (mapcar #'downcase (split-string deps nil 'omit-nulls))))
 
 (defconst emake-package-archive-master-alist
-  '(("melpa" . "http://melpa.org/packages/")
-    ("gnu" . "http://elpa.gnu.org/packages/"))
+  '(("gnu"          . "https://elpa.gnu.org/packages/")
+    ("melpa"        . "https://melpa.org/packages/")
+    ("melpa-stable" . "https://stable.melpa.org/packages/")
+    ("org"          . "http://orgmode.org/elpa/"))
   "Archive definition alist.
 Key is the string name of the archive.
 Value is the URL at which the archive is hosted.")

commit c3fbf84b113150177be276cbac8d61344ace80fb

Don't try to access archives over HTTPS
Regression introduced in 9a2c7716f41a5bff6723f3920d9e025633cb6796.

emake.el
@@ -87,9 +87,9 @@ list of arguments for that format string."
     (mapcar #'downcase (split-string deps nil 'omit-nulls))))
 
 (defconst emake-package-archive-master-alist
-  '(("gnu"          . "https://elpa.gnu.org/packages/")
-    ("melpa"        . "https://melpa.org/packages/")
-    ("melpa-stable" . "https://stable.melpa.org/packages/")
+  '(("gnu"          . "http://elpa.gnu.org/packages/")
+    ("melpa"        . "http://melpa.org/packages/")
+    ("melpa-stable" . "http://stable.melpa.org/packages/")
     ("org"          . "http://orgmode.org/elpa/"))
   "Archive definition alist.
 Key is the string name of the archive.