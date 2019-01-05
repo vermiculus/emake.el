3ba4bd993320cd9d902b3c931042433b6fa67061
Sean Allred <code@seanallred.com>
2019-01-05 09:23:18 -0600

    Add copyright to file `emake.mk'

--- emake.mk
+++ emake.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2018-2019  Sean Allred
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Minimum configuration
 #   PACKAGE_BASENAME: the basename that prefixes all Lisp files in this package
 #   EMAKE_SHA1:       the version of emake you want to use
